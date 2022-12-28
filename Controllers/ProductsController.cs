ca99eec33b3d6f4c5bab3944a939afd4ea89759e
Grzegorz <grzegorz.piatek@scan-it.pl>
2022-12-28 15:37:40 +0100

    Repair filter form states, add alcoholType filter

--- Controllers/ProductsController.cs
+++ Controllers/ProductsController.cs
@@ -3,7 +3,7 @@ using Microsoft.AspNetCore.Mvc.Rendering;
 using Microsoft.EntityFrameworkCore;
 using PWProject.Migrations;
 using PWProject.Models;
-using AlcoholType = PWProject.Migrations.AlcoholType;
+using AlcoholType = PWProject.Models.AlcoholType;
 
 namespace PWProject.Controllers
 {
@@ -20,56 +20,139 @@ namespace PWProject.Controllers
         public IActionResult Index()
         {
             var sortColumn = Request.Query["sortColumn"];
+            var sortOrder = Request.Query["sortOrder"];
             var filterValue = Request.Query["filterValue"];
-            AlcoholType alcoholType;
-            List<Product> products = new List<Product>();
-            try
+            var alcoholType = Request.Query["alcoholType"];
+            // Build the query using LINQ methods and execute the query only when the results need to be materialized.
+            var products = GetProductsFromDatabase(filterValue, sortColumn, sortOrder, alcoholType);
+            // Return the filtered and sorted products to the view.
+            var listProducts = products.ToList();
+            return View(listProducts);
+        }
+
+        private IQueryable<Product> GetProductsFromDatabase(string filterValue, string sortColumn, string sortOrder, string alcoholType)
+        {
+            // Build the query using LINQ methods.
+            var query = _context.Products
+                .Include(p => p.Producer)
+                .AsQueryable();
+            if (!string.IsNullOrEmpty(filterValue))
             {
-                alcoholType = (AlcoholType)Enum.Parse(typeof(AlcoholType), filterValue);
-            }
-            catch (ArgumentException)
-            {
-                alcoholType = default;
+                query = query.Where(p => p.Name.Contains(filterValue) || p.Description.Contains(filterValue) || p.Producer.Name.Contains(filterValue));
             }
             if (!string.IsNullOrEmpty(sortColumn))
             {
                 if (sortColumn == "Name")
                 {
-                    products = _context.Products
-                        .Include(p => p.Producer)
-                        .OrderBy(p => p.Name)
-                        .ToList();
+                    if (sortOrder == "asc")
+                    {
+                        query = query.OrderBy(p => p.Name);
+                    }
+                    else
+                    {
+                        query = query.OrderByDescending(p => p.Name);
+                    }
                 }
                 else if (sortColumn == "Producer.Name")
                 {
-                    products = _context.Products
-                        .Include(p => p.Producer)
-                        .OrderBy(p => p.Producer.Name)
-                        .ToList();
+                    if (sortOrder == "asc")
+                    {
+                        query = query.OrderBy(p => p.Producer.Name);
+                    }
+                    else
+                    {
+                        query = query.OrderByDescending(p => p.Producer.Name);
+                    }
                 }
             }
-
-            if (!string.IsNullOrEmpty(filterValue))
+            if (alcoholType != "" && alcoholType != null)
             {
-                products = _context.Products
-                    .Include(p => p.Producer)
-                    .Where(
-                        p => p.Name.Contains(filterValue) ||
-                        p.Producer.Name.Contains(filterValue) ||
-                        p.AlcoholType.Equals(alcoholType)
-                    )
-                    .ToList();
-            }
+                AlcoholType selectedAlcoholType;
 
-            if (!products.Any())
-            {
-                products = _context.Products
-                    .Include(p => p.Producer)
-                    .ToList();
+                // Try to parse the filterValue as an AlcoholType enum value.
+                // If it's not a valid AlcoholType value, set it to the default value.
+                Enum.TryParse(alcoholType, out selectedAlcoholType);
+                query = query.Where(p => p.AlcoholType == selectedAlcoholType);
             }
+            ViewData["AlcoholTypes"] = Enum.GetValues(typeof(AlcoholType)).Cast<AlcoholType>();
 
-            return View(products);
+            // Execute the query and materialize the results.
+            return query;
         }
+
+        private IQueryable<Product> ApplyAlcoholTypeFiltering(IQueryable<Product> products, string? alcoholType)
+        {
+
+            if (alcoholType != null)
+            {
+                AlcoholType selectedAlcoholType;
+
+                // Try to parse the filterValue as an AlcoholType enum value.
+                // If it's not a valid AlcoholType value, set it to the default value.
+                if (!Enum.TryParse(alcoholType, out selectedAlcoholType))
+                {
+                    alcoholType = default;
+                }
+                products = products.Where(p => p.AlcoholType.Equals(alcoholType));
+            }
+
+            return products;
+        }
+
+        //// GET: Products
+        //public IActionResult Index()
+        //{
+        //    var sortColumn = Request.Query["sortColumn"];
+        //    var filterValue = Request.Query["filterValue"];
+        //    AlcoholType alcoholType;
+        //    List<Product> products = new List<Product>();
+        //    try
+        //    {
+        //        alcoholType = (AlcoholType)Enum.Parse(typeof(AlcoholType), filterValue);
+        //    }
+        //    catch (ArgumentException)
+        //    {
+        //        alcoholType = default;
+        //    }
+        //    if (!string.IsNullOrEmpty(sortColumn))
+        //    {
+        //        if (sortColumn == "Name")
+        //        {
+        //            products = _context.Products
+        //                .Include(p => p.Producer)
+        //                .OrderBy(p => p.Name)
+        //                .ToList();
+        //        }
+        //        else if (sortColumn == "Producer.Name")
+        //        {
+        //            products = _context.Products
+        //                .Include(p => p.Producer)
+        //                .OrderBy(p => p.Producer.Name)
+        //                .ToList();
+        //        }
+        //    }
+
+        //    if (!string.IsNullOrEmpty(filterValue))
+        //    {
+        //        products = _context.Products
+        //            .Include(p => p.Producer)
+        //            .Where(
+        //                p => p.Name.Contains(filterValue) ||
+        //                p.Producer.Name.Contains(filterValue) ||
+        //                p.AlcoholType.Equals(alcoholType)
+        //            )
+        //            .ToList();
+        //    }
+
+        //    if (!products.Any())
+        //    {
+        //        products = _context.Products
+        //            .Include(p => p.Producer)
+        //            .ToList();
+        //    }
+
+        //    return View(products);
+        //}
 
         // GET: Products/Details/5
         public async Task<IActionResult> Details(int? id)
---
+++ Models/ProductIndexViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace PWProject.Models
+{
+    public class ProductIndexViewModel
+    {
+        public IEnumerable<Product> Products { get; set; }
+        public AlcoholType SelectedAlcoholType { get; set; }
+    }
+
+}
